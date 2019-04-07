4ef91b9b51cca674e79ac0461f8565b48416d3ca
Markus Lindberg <markus.lindberg073@gmail.com>
2019-04-07 20:31:55 +0300

    Change ordering labels to newest and oldest

---
+++ app/src/androidTest/java/com/example/observationdatabase/ObservationInputTest.kt
@@ -0,0 +1,5 @@
+package com.example.observationdatabase
+
+import org.junit.Assert.*
+
+class ObservationInputTest
---
+++ app/src/androidTest/java/com/example/observationdatabase/ObservationRepositoryTest.kt
@@ -0,0 +1,30 @@
+package com.example.observationdatabase
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ObservationRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getObservations() {
+    }
+
+    @Test
+    fun insertObservation() {
+    }
+
+    @Test
+    fun selector() {
+    }
+}
